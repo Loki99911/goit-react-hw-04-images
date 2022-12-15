649e7dc2f13646116e4325c58666c08f52fc77f9
Loki99911 <nikita_kresik@i.ua>
2022-12-15 16:26:04 +0200

    1

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,43 +1,75 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
+
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.hendlePressEsc);
-  }
+export const Modal = funcCloseClick => {
+  useEffect(() => {
+    const hendlePressEsc = event => {
+      if (event.code === 'Escape') {
+        console.log('EscON');
+        window.addEventListener('keydown', hendlePressEsc);
+      }
+    };
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.hendlePressEsc);
-  }
+    return () => {
+      window.removeEventListener('keydown', hendlePressEsc);
+      console.log('EscOFF');
+    };
+  }, []);
 
-  hendlePressEsc = event => {
-    if (event.code === 'Escape') {
-      this.props.funcCloseClick();
-    }
-  };
-
-  hendleClickBackdrop = event => {
+  const hendleClickBackdrop = event => {
     if (event.currentTarget === event.target) {
-      this.props.funcCloseClick();
+      funcCloseClick();
     }
   };
 
-  render() {
-    return createPortal(
-      <div className="Overlay" onClick={this.hendleClickBackdrop}>
-        <div className="Modal">
-          <img src={this.props.url} alt={this.props.text} />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <div className="Overlay" onClick={hendleClickBackdrop}>
+      <div className="Modal">
+        <img src={this.props.url} alt={this.props.text} />
+      </div>
+    </div>,
+    modalRoot
+  );
+};
 
 Modal.propTypes = {
   url: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
   funcCloseClick: PropTypes.func.isRequired,
 };
+
+// export class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.hendlePressEsc);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.hendlePressEsc);
+//   }
+
+//   hendlePressEsc = event => {
+//     if (event.code === 'Escape') {
+//       this.props.funcCloseClick();
+//     }
+//   };
+
+//   hendleClickBackdrop = event => {
+//     if (event.currentTarget === event.target) {
+//       this.props.funcCloseClick();
+//     }
+//   };
+
+//   render() {
+//     return createPortal(
+//       <div className="Overlay" onClick={this.hendleClickBackdrop}>
+//         <div className="Modal">
+//           <img src={this.props.url} alt={this.props.text} />
+//         </div>
+//       </div>,
+//       modalRoot
+//     );
+//   }
+// }
